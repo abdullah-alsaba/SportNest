34acfecee4c05ebc9c2dde9d6b62489336566fe7
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-05-21 18:29:19 +0600

    MongoDB Connected

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,14 @@
+import dns from 'dns'
 import mongoose from 'mongoose'
+
+dns.setServers(['8.8.8.8', '8.8.4.4'])
+dns.setDefaultResultOrder('ipv4first')
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 15000,
+    })
     console.log(`MongoDB connected: ${conn.connection.host}`)
   } catch (error) {
     console.error(`MongoDB error: ${error.message}`)
